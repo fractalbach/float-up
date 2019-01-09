6c54f00cc4f4ab8039948db0f84a354f7e3bfc45
fractalbach <fractalbach@gmail.com>
2019-01-08 21:03:27 -0800

    replaced service worker
    removed the old service and replaced it with the new one that was 
    previously in scripts/

--- sw.js
+++ sw.js
@@ -1,84 +1,48 @@
-// Service Workers
-
-    const CACHE_NAME = 'float-up-cache-v1.2';
-
-  // Create a cache of the main pages for the game.
-  // Things to run when the service worker is first installed.
-  // Creates a cache of the specified URLs
-  self.addEventListener('install', function(event) {
-      var urlsToCache = [
-        'index.html',
-        'main.js',
-        'debugger.js',
-        'betterControls.js'
-      ];
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
 
 
-  // Enables Updates for service workers.
-  // Loop through caches that aren't on the whitelisted and delete them.
-  // Uses the names from the global cache list variable.
-  self.addEventListener('activate', function(event) {
-    var cacheWhitelist = [];
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
+const CACHE_NAME = 'float-up-cache-v1.3';
+
+// Credits:
+// The following code is primarily derived from:
+// https://www.pwabuilder.com/serviceworker
+// "Offline copy of pages"
+
+//Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener('install', function(event) {
+  var indexPage = new Request('index.html');
+  event.waitUntil(
+    fetch(indexPage).then(function(response) {
+      return caches.open(CACHE_NAME).then(function(cache) {
+        console.log('opened cache'+ response.url);
+        return cache.put(indexPage, response);
+      });
+  }));
+});
 
 
-
-
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
-          var fetchRequest = event.request.clone();
-
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-
-              return response;
-            }
-          );
-        })
-      );
-  });
+//If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener('fetch', function(event) {
+  var updateCache = function(request){
+    return caches.open(CACHE_NAME).then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log('add page to offline cache'+response.url)
+        return cache.put(request, response);
+      });
+    });
+  };
+  event.waitUntil(updateCache(event.request));
+  //Check to see if you have it in the cache
+  //Return response
+  //If not in the cache, then return error page
+  event.respondWith(
+   fetch(event.request).catch(function(error) {
+     console.log( 'Network request Failed. Serving content from cache: ' + error );
+     return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(event.request).then(function (matching) {
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+          return report
+        });
+      });
+    })
+  );
+})

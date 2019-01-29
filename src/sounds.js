a47dbb69ae73ceb13a8a8ed20df8853acf17479f
fractalbach <fractalbach@gmail.com>
2019-01-28 23:07:42 -0800

    sounds: now loads 3x same sound, plays in rotation
    - should allow for multiple overlapping sounds AND be able to work on 
    mobile android

--- src/sounds.js
+++ src/sounds.js
@@ -2,11 +2,16 @@ const GameSoundEffects = (function(){
 
     const BALLOON_POP_FILEPATH = "sounds/balloon_pop.wav"
 
+    let balloonIndex = 0
+    let balloonList = [
+        new Audio(BALLOON_POP_FILEPATH),
+        new Audio(BALLOON_POP_FILEPATH),
+        new Audio(BALLOON_POP_FILEPATH)
+    ]
+
     function playBalloonPopSound() {
-        let audio = new Audio(BALLOON_POP_FILEPATH)
-        audio.addEventListener('loadeddata', ()=>{
-            audio.play()
-        })
+        balloonIndex = (balloonIndex + 1) % (balloonList.length)
+        balloonList[balloonIndex].play()
     }
 
     return {
